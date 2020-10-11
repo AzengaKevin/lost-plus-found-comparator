b52746dd4312714ba3d221a8871ac0308a3f84e1
Azenga Kevin <azenga.kevin7@gmail.com>
2020-10-11 20:48:57 +0300

    Wip, changed the table seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,10 +13,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         factory(User::class)->create([
-            'name' =>'Azenga Kevin',
-            'email' => 'azenga.kevin7@gmail.com',
-            'phone_number' => '+254700016349',
-            'national_identification_number' => '33708342',
+            'name' =>'Super Admin',
+            'email' => 'admin@gmail.com',
+            'phone_number' => '+254700000000',
+            'national_identification_number' => '00000000',
         ]);
     }
 }
