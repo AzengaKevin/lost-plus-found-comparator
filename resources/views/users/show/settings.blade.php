60dc9b0e6d8b33df8a0433348beaa9cfedb636ab
Azenga Kevin <azenga.kevin7@gmail.com>
2020-11-08 14:50:25 +0300

    Wip, user can successfully update their password

--- resources/views/users/show/settings.blade.php
+++ resources/views/users/show/settings.blade.php
@@ -5,38 +5,7 @@
 @section('content')
 <div class="container">
 
-    <div class="row">
-        <div class="col-md-4">
-            <h4 class="text-gray-900 font-weight-bold">Update Password</h4>
-            <span>Ensure your account is using a long, random password to stay secure.</span>
-        </div>
-        <div class="col-md-8">
-            <div class="card bg-white rounded shadow-sm">
-                <div class="card-body">
-                    <div class="card-text">
-                        <div class="form-group">
-                            <label for="currentPassword" class="text-gray-800 font-weight-bold">Current Password</label>
-                            <input type="password" name="current_password" id="currentPassword" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="newPassword" class="text-gray-800 font-weight-bold">New Password</label>
-                            <input type="password" name="new_password" id="newPassword" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="passwordConfirmation" class="text-gray-800 font-weight-bold">Confirm
-                                Password</label>
-                            <input type="password" name="password_confirmation" id="passwordConfirmation"
-                                class="form-control">
-                        </div>
-
-                        <div class="clearfix">
-                            <button type="button" class="btn btn-dark text-uppercase float-right">Save</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @livewire('users.update-password');
 
     <hr class="my-5">
 
