acbdcb4998348c290e240eaf7d759855f69d605b
Azenga Kevin <azenga.kevin7@gmail.com>
2021-02-03 05:31:14 +0300

    Wip, installing and configuring scout using algolia driver

--- app/Report.php
+++ app/Report.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Report extends Model
 {
     use SoftDeletes;
+    use Searchable;
 
     protected $fillable = [
         'officer_id',
